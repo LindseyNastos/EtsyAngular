e7d2e45cfdada9448b0959c9c79fbe76c8d822fa
LindseyNastos <lindseynastos@gmail.com>
2016-02-25 18:01:28 -0800

    Added productServices.ts back in

---
+++ EtsyAngular/Angular/AJSServices/ProductServices.ts
@@ -0,0 +1,27 @@
+﻿namespace Etsy.ProductServices {
+
+    export class ProductService {
+        private ProductService;
+
+        public listProducts() {
+            return this.ProductService.query().$promise;
+        }
+
+        public save(product) {
+            return this.ProductService.save(product).$promise;
+        }
+
+        public getProduct(id) {
+            return this.ProductService.get({ id: id });
+        }
+
+        public deleteProduct(id) {
+            return this.ProductService.delete({ id: id }).$promise;
+        }
+
+        constructor($resource: ng.resource.IResourceService) {
+            this.ProductService = $resource('/api/products/:id');
+        }
+    }
+    angular.module('EtsyApp').service('productService', ProductService);
+}
